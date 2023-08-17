29198d7554f4481b116edb00c93a0e74f725a168
Aanshi-Singh <aanshisingh04022002@gmail.com>
2023-08-17 15:21:46 +0530

    single non duplicate

--- Step 4/12_singleNonDuplicate.cpp
+++ Step 4/12_singleNonDuplicate.cpp
@@ -2,30 +2,11 @@
 int singleNonDuplicate(vector<int>& arr)
 {
 	// Write your code here
-	int n = arr.size();
-	if(n == 1) return arr[0];
-	if(arr[0]!=arr[1]) return arr[0];
-	if (arr[n-1]!=arr[n-2]) return arr[n-1];
-
-	int l = 1;
-	int r = n-2;
-	
-	while(l<=r){
-
-		int mid = (l+r)/2;
-		if(arr[mid]!= arr[mid+1] && arr[mid-1]!=arr[mid]){
-			return arr[mid];
-		}
-
-		if((mid%2 == 1 && arr[mid] == arr[mid-1]) ||
-		 (mid%2 == 0 && arr[mid]== arr[mid+1]))
-			l = mid+1;
-		
-		else {
-			r = mid-1;
-		}
+	int xorr = 0;
+	for(int i = 0; i<arr.size(); i++){
+		xorr^=arr[i];
 	}
-	return -1;
+	return xorr;
 }
 
 //--------------------------------------------------------------
